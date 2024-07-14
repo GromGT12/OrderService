c45efa6ba57e8caf729c378e8697fb832bd8d6ad
Maks <122127608+GromGT12@users.noreply.github.com>
2024-07-14 12:10:58 +0200

    The application works correctly

---
+++ src/main/java/com/sweet_bites_delivery_service/config/KafkaConfig.java
@@ -0,0 +1,42 @@
+package com.sweet_bites_delivery_service.config;
+
+import com.sweet_bites_delivery_service.dto.CartItemDTO;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public ProducerFactory<String, CartItemDTO> kafkaProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, org.apache.kafka.common.serialization.StringSerializer.class);
+        configProps.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    @Bean
+    public KafkaTemplate<String, CartItemDTO> kafkaProducerTemplate() {
+        return new KafkaTemplate<>(kafkaProducerFactory());
+    }
+
+    @Bean
+    public NewTopic cartItemTopic() {
+        return new NewTopic("cart-item-topic", 1, (short) 1);
+    }
+}
